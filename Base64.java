9cec392ca511ceb7093d7848663312644df18a9c
Shafiq Sadat <ShafiqSadat@users.noreply.github.com>
2019-02-28 12:32:10 +0430

    Update and rename base64.java to Base64.java
    Summarized! 🤓

--- Base64.java
+++ Base64.java
@@ -1,33 +1,26 @@
-
-package Code;
 import java.util.Scanner;
 import javax.xml.bind.DatatypeConverter;
-public class base64 {
+public class Base64 {
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
         System.out.println("Hi Welcome \n1 » Strint to base64\n2 » base64 to String");
-        int code = input.nextInt();
-        switch(code){
+        switch(input.nextInt()){
             case 1:
             System.out.println("Ok now send me String :");
-            String tobase = input.next();
-            encode(tobase);
+            encode(input.next());
             break;
             case 2:
             System.out.println("Ok now send me base64 :");
-            String tostring = input.next();
-            decode(tostring);
+            decode(input.next());
             break;
             default:
                 System.out.println("404 :/");
         }
     }
     static void encode(String code){
-        String encode = DatatypeConverter.printBase64Binary(code.getBytes());
-        System.out.println(encode);
+        System.out.println(DatatypeConverter.printBase64Binary(code.getBytes()));
     }
     static void  decode(String code){
-        String decode = new String (DatatypeConverter.parseBase64Binary(code));
-        System.out.println(decode);
+        System.out.println(new String (DatatypeConverter.parseBase64Binary(code)));
     }
 }
